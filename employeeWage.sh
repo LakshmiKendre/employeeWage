caf7417ba4a4875c2c6e58cfe4b2dc98dbe1d2d1
admin <lakshmi.patil.nine@gmail.com>
2020-01-21 17:46:30 +0530

    added: Calculate salary for month

--- employeeWage.sh
+++ employeeWage.sh
@@ -1,20 +1,34 @@
 #!/bin/bash -x
 
 echo "Welcome to EmployeeWage"
+# Declaring constants
 WAGE_PER_HOUR=20
 FULL_DAY_HOUR=8
 HALF_DAY_HOUR=4
 
-attendance=$((RANDOM%3))
+# Declaring variables
+day=1
+salary=0
 
-case $attendance in
-0 )
-	echo "Daily wage = 0" 
-	;;
-1 )
-	echo "Daily wage (full-day)= " $((WAGE_PER_HOUR * FULL_DAY_HOUR))
-	;;
-2 )
-	echo "Daily wage (half-day) = "$((WAGE_PER_HOUR * HALF_DAY_HOUR))
-	;;
-esac
+# calculating monthly salary
+while [ $day -le 20 ]
+do
+	attendance=$((RANDOM%3))
+	case $attendance in
+	0 )
+		salary=$((salary + 0)) 
+		;;
+	1 )
+		salary=$((salary + $((WAGE_PER_HOUR * FULL_DAY_HOUR)) ))
+		;;
+	2 )
+		salary=$((salary + $((WAGE_PER_HOUR * HALF_DAY_HOUR)) ))
+		;;
+	esac
+let day++
+done
+
+# printing monthly salary
+echo "monthly salary is: " $salary
+
+
