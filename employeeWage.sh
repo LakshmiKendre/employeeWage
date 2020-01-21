0d0649e91da1c16d5eb7c6547d9d210af9d45e63
admin <lakshmi.patil.nine@gmail.com>
2020-01-21 16:52:58 +0530

    added: Part-time Employee and Calculate wage

--- employeeWage.sh
+++ employeeWage.sh
@@ -3,11 +3,17 @@
 echo "Welcome to EmployeeWage"
 WAGE_PER_HOUR=20
 FULL_DAY_HOUR=8
-if [ $((RANDOM%2)) -eq 1 ]
+HALF_DAY_HOUR=4
+attendance=$((RANDOM%3))
+if [ $attendance -eq 1 ]
 then
 	echo Present
-	echo "Daily wage = " $((WAGE_PER_HOUR * FULL_DAY_HOUR))
-else
+	echo "Daily wage (full-day)= " $((WAGE_PER_HOUR * FULL_DAY_HOUR))
+elif [ $attendance -eq 0 ]
+then
 	echo Absent
 	echo "Daily wage = 0" 
+else
+	echo Present
+	echo "Daily wage (half-day) = "$((WAGE_PER_HOUR * HALF_DAY_HOUR))
 fi
