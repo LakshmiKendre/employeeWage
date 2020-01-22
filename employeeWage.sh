6363880c14ef83a2175eb21e6a39b5ce9163cab2
admin <lakshmi.patil.nine@gmail.com>
2020-01-22 10:06:43 +0530

    added: function to refactor code

--- employeeWage.sh
+++ employeeWage.sh
@@ -11,6 +11,31 @@ day=1
 hour=0
 salary=0
 
+Employee_wage_for_hour()
+{
+	
+	if [ $attendance -eq 0 ]
+	then
+		hour=$((hour + FULL_DAY_HOUR))
+		if [ $hour -gt 100 ]
+		then
+			difference=$((hour - 100))
+			salary=$((salary + $((WAGE_PER_HOUR * FULL_DAY_HOUR)) - $((WAGE_PER_HOUR * difference)) ))
+		else
+			salary=$((salary + $((WAGE_PER_HOUR * FULL_DAY_HOUR)) ))
+		fi
+	else
+		hour=$((hour + HALF_DAY_HOUR))
+		if [ $hour -gt 100 ]
+		then
+			difference=$((hour - 100))
+			salary=$((salary + $((WAGE_PER_HOUR * HALF_DAY_HOUR)) - $((WAGE_PER_HOUR * difference)) ))
+		else
+			salary=$((salary + $((WAGE_PER_HOUR * HALF_DAY_HOUR)) ))
+		fi
+	fi
+}
+
 # calling function to calculating 100 hours salary
 while [ $day -le 20 -a $hour -lt 100 ]
 do
@@ -27,19 +52,7 @@ let day++
 
 done
 
-Employee_Wage_for_hour()
-{
-	
-	if [ $attendance -eq 0 ]
-	then
-		hour=$((hour + FULL_DAY_HOUR))
-		salary=$((salary + $((WAGE_PER_HOUR * FULL_DAY_HOUR)) ))
-	else
-		hour=$((hour + HALF_DAY_HOUR))
-		salary=$((salary + $((WAGE_PER_HOUR * HALF_DAY_HOUR)) ))
 
-	fi
-}
 # printing 100 hours salary
 echo "monthly salary is: " $salary
 
