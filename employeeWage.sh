a832a75ee11e6a83ee34530baf4e8b47f4550a62
admin <lakshmi.patil.nine@gmail.com>
2020-01-22 19:17:26 +0530

    refactor: added function to get working hours

--- employeeWage.sh
+++ employeeWage.sh
@@ -3,57 +3,53 @@
 echo "Welcome to EmployeeWage"
 # Declaring constants
 WAGE_PER_HOUR=20
-FULL_DAY_HOUR=8
-HALF_DAY_HOUR=4
 
 # Declaring variables
 day=1
 hour=0
-salary=0
+total_salary=0
 
-Employee_wage_for_hour()
+# using function getting work hours of employee
+get_working_hour()
 {
-	
-	if [ $attendance -eq 0 ]
-	then
-		hour=$((hour + FULL_DAY_HOUR))
-		if [ $hour -gt 100 ]
-		then
-			difference=$((hour - 100))
-			salary=$((salary + $((WAGE_PER_HOUR * FULL_DAY_HOUR)) - $((WAGE_PER_HOUR * difference)) ))
-		else
-			salary=$((salary + $((WAGE_PER_HOUR * FULL_DAY_HOUR)) ))
-		fi
-	else
-		hour=$((hour + HALF_DAY_HOUR))
-		if [ $hour -gt 100 ]
-		then
-			difference=$((hour - 100))
-			salary=$((salary + $((WAGE_PER_HOUR * HALF_DAY_HOUR)) - $((WAGE_PER_HOUR * difference)) ))
-		else
-			salary=$((salary + $((WAGE_PER_HOUR * HALF_DAY_HOUR)) ))
-		fi
-	fi
+	attendance=$((RANDOM%2))
+	case $attendance in
+		0)
+			working_hour=8
+			;;
+		1)
+			working_hour=4
+			;;
+	esac
+	echo $working_hour
+}
+
+#calculating 100 hours salary of employee
+Employee_wage_for_hour()
+{	
+	work_hour=$1
+	salary=$((WAGE_PER_HOUR * work_hour))
+	echo $salary
 }
 
 # calling function to calculating 100 hours salary
 while [ $day -le 20 -a $hour -lt 100 ]
 do
-	attendance=$((RANDOM%2))
-	case $attendance in
-	0 )
-		Employee_wage_for_hour
-		;;
-	1 )
-		Employee_wage_for_hour
-		;;
-	esac
+	work_hrs="$(get_working_hour)"
+	total_salary=$((total_salary + "$(Employee_wage_for_hour $work_hrs)"))
+	hour=$((hour + work_hrs))
 let day++
 
 done
 
 
 # printing 100 hours salary
-echo "monthly salary is: " $salary
-
+if [ $hour -gt 100 ]
+then
+	diff=$((hour-100))
+	total_salary=$((total_salary + ${arr[day]} - $((WAGE_PER_HOUR * $diff )) ))
+	echo "Total salary is: " $total_salary
+else
+	echo "Total salary is: " $total_salary
+fi
 
