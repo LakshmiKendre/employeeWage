9ce335cec147c5a78899f3f302da815dfcf9243b
admin <lakshmi.patil.nine@gmail.com>
2020-01-27 10:17:23 +0530

    modified: removed unnecessary echo statements

--- employeeWage.sh
+++ employeeWage.sh
@@ -5,9 +5,7 @@ WAGE_PER_HOUR=20
 FULL_DAY_HOUR=8
 if [ $((RANDOM%2)) -eq 1 ]
 then
-	echo Present
-	echo "Daily wage = " $((WAGE_PER_HOUR * FULL_DAY_HOUR))
+	echo "(Present)Daily wage = " $((WAGE_PER_HOUR * FULL_DAY_HOUR))
 else
-	echo Absent
-	echo "Daily wage = 0" 
+	echo "(Absent)Daily wage = 0" 
 fi
